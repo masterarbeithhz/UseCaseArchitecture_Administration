390880f135ac601aa7234c9cf752b6d04eaa1218
Tobias <masterarbeithhz@gmail.com>
2021-01-02 16:26:30 +0100

    aaa

--- image_files/login/home.php
+++ image_files/login/home.php
@@ -1,6 +1,6 @@
 <?php
 include "config.php";
-include("envVariables.php");
+//include("envVariables.php");
 // Check user login or not
 if (!isset($_SESSION['uname'])) {
     header('Location: index.php');
